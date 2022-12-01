66bfc2ef1dda3b8f0fb71282347bed3bee9fbcb2
HannaBerggren <hannajberggren@gmail.com>
2022-12-01 08:17:42 +0000

    add game variables

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -8,21 +8,5 @@ const playerScore = document.getElementById("user");
 const computerScore = document.getElementById("comp");
 const winBox = document.getElementById("win-box");
 const loseBox = document.getElementById("lose-box");
-const play = document.getElementById("play-again");
-
-const main = () => {
-	rockB.addEventListener("click", function() {
-		playGame("rock");
-	});
-}
-
-paperB.addEventListener("click", function() {
-	playGame("paper");
-});
-
-scissorsB.addEventListener("click", function() {
-	playGame("scissors");
-});
-
-main();
-
+const play = document.getElementById("play-game");
+const playA = document.getElementById("play-again");
